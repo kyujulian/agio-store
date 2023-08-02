8c71cb3ffc796f8ae7499348cb53457d5364d29f
Julian <liannsd3@gmail.com>
2023-08-02 00:38:47 -0300

    first collection

---
+++ src/lib/shopify/queries/collection.ts
@@ -0,0 +1,19 @@
+import productFragment from '../fragments/product'
+export const getCollectionProductsQuery = /* GraphQL */ `
+  query getCollectionsProducts(
+    $handle: String!
+    $sortKey: ProductCollectionSortKeys
+    $reverse: Boolean
+  ) {
+    collection(handle: $handle) {
+      products(sortKey: $sortKey, reverse: $reverse, first: 100) {
+        edges {
+          node {
+            ...product
+          }
+        }
+      }
+    }
+  }
+  ${productFragment}
+`
