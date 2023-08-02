6b2925c645bf5c7c8eeba1adffdd23fb8d27ddbf
Julian <liannsd3@gmail.com>
2023-08-02 17:35:29 -0300

    got products displayed and fixed api endpoint link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Montserrat } from "@next/font/google";
 // import { GridItem } from "components/grid";
+import { getCollectionProducts } from "@/lib/shopify";
 
 export const metadata = {
   description:
@@ -9,12 +10,24 @@ export const metadata = {
   },
 };
 
-export default function Home() {
+export default async function Home() {
+  const homepageItems = await getCollectionProducts({
+    collectionId: "Caps",
+  });
   return (
     <>
       <main className="flex h-screen">
-        <div className=" h-full w-full flex justify-center  items-center ">
+        <div className=" h-full flex-col w-full flex justify-center  items-center ">
           <h1 className="text-[42px] font-bold"> Agio StoreFront </h1>
+          <ul className="text-slate-200 p-7 w-[500px] rounded-md border-[1px] border-slate-200">
+            {homepageItems.map((item) => (
+              <li className="py-5">
+                <b>Product Title:</b> {item.title} <br />
+                <b>Description:</b>
+                {item.description}
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-1/graphql.json';
+export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-07/graphql.json';
 export const TAGS = {
   collections: 'collections',
   products: 'products'
