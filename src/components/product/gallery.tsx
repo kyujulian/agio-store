e17c8dfdb2c10ab69575d0f5bc77befcad1dd520
Julian <liannsd3@gmail.com>
2023-08-05 16:50:14 -0300

    image navigation

--- src/components/product/gallery.tsx
+++ src/components/product/gallery.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 import { createUrl } from '@/lib/utils';
+import { GridTileImage } from '../grid/tile';
 
 export default function Gallery({
   images,
@@ -25,8 +26,8 @@ export default function Gallery({
 
   const previousSearchParams = new URLSearchParams(searchParams.toString());
   const previousImageIndex =
-    imageIndex - 1 < images.length ? imageIndex - 1 : 0;
-  nextSearchParams.set('image', previousImageIndex.toString());
+    imageIndex === 0 ? images.length - 1 : imageIndex - 1;
+  previousSearchParams.set('image', previousImageIndex.toString());
   const previousUrl = createUrl(pathname, previousSearchParams);
 
   const buttonClassName =
@@ -46,19 +47,60 @@ export default function Gallery({
         )}
         {images.length > 1 ? (
           <div className="absolute bottom-[15%] flex w-full justify-center">
-            <div className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur dark:border-black dark:bg-neutral-900/80"></div>
-            <Link
-              aria-label="Previous product image"
-              href={previousUrl}
-              className={buttonClassName}
-              scroll={false}
-            >
-              <ArrowLeftIcon className="h-5" />
-            </Link>
+            <div className="mx-auto flex h-11 items-center rounded-full border border-white bg-neutral-50/80 text-neutral-500 backdrop-blur dark:border-black dark:bg-neutral-900/80">
+              <Link
+                aria-label="Previous product image"
+                href={previousUrl}
+                className={buttonClassName}
+                scroll={false}
+              >
+                <ArrowLeftIcon className="h-5" />
+              </Link>
+              <div className="mx-1 h-6 w-px bg-neutral-500"></div>
+              <Link
+                aria-label="Next product image"
+                href={nextUrl}
+                className={buttonClassName}
+                scroll={false}
+              >
+                <ArrowRightIcon className="h-5" />
+              </Link>
+            </div>
           </div>
         ) : null}
       </div>
-      <div>GalleryComponent</div>
+
+      {images.length > 1 ? (
+        <ul className="my-12 flex items-center justify-center gap-2 overflow-auto py-1 lg:mb-0">
+          {images.map((image, index) => {
+            const isActive = index === imageIndex;
+            const imageSearchParams = new URLSearchParams(
+              searchParams.toString(),
+            );
+
+            imageSearchParams.set('image', index.toString());
+
+            return (
+              <li key={image.src} className="h-auto w-20">
+                <Link
+                  aria-label="Enlarge product image"
+                  href={createUrl(pathname, imageSearchParams)}
+                  scroll={false}
+                  className="h-full w-full"
+                >
+                  <GridTileImage
+                    alt={image.altText}
+                    src={image.src}
+                    width={80}
+                    height={80}
+                    active={isActive}
+                  />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
     </>
   );
 }
