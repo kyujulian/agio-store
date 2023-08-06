cfb373e3ca40bbcac659cddba6c72e57f41f7734
Julian <liannsd3@gmail.com>
2023-08-06 12:07:14 -0300

    footer border fix

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,22 +1,22 @@
-import Link from "next/link";
-import LogoSquare from "../logo-square";
-import { SITE_NAME, COMPANY_NAME } from "@/constants";
-import { getMenu } from "@/lib/shopify";
-import { Suspense } from "react";
-import FooterMenu from "./footer-menu";
+import Link from 'next/link';
+import LogoSquare from '../logo-square';
+import { SITE_NAME, COMPANY_NAME } from '@/constants';
+import { getMenu } from '@/lib/shopify';
+import { Suspense } from 'react';
+import FooterMenu from './footer-menu';
 
-import GithubIcon from "@/components/icons/github";
+import GithubIcon from '@/components/icons/github';
 
 export default async function Footer() {
   const skeleton = `w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700`;
   const currentYear = new Date().getFullYear();
-  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : "");
-  const menu = await getMenu("footer");
-  const copyrightName = COMPANY_NAME || SITE_NAME || "";
+  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
+  const menu = await getMenu('footer');
+  const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
-    <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 xl:px-0 ">
+    <footer className="border-t border-neutral-200 text-sm text-neutral-500 dark:border-neutral-700 dark:text-neutral-400">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6  px-6 py-12 text-sm  md:flex-row md:gap-12 md:px-4 xl:px-0 ">
         <div>
           <Link
             className=" flex items-center gap-2 text-black dark:text-white md:pt-1"
@@ -54,15 +54,15 @@ export default async function Footer() {
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 xl:px-0">
           <p>
             &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith(".")
-              ? "."
-              : ""}{" "}
+            {copyrightName.length && !copyrightName.endsWith('.')
+              ? '.'
+              : ''}{' '}
             All rights reserved.
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p> Designed as Portfolio project</p>
           <p className="md:ml-auto">
-            by{" "}
+            by{' '}
             <a href="" className="text-black dark:text-white">
               ♦ Julian
             </a>
